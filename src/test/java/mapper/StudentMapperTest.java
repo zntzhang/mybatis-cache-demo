1aee9426f74ae92a84d0e859785a5dcae2083aa4
张涛 <zhangtao@olymtech.com>
2020-07-13 09:19:49 +0800

    update

--- src/test/java/mapper/StudentMapperTest.java
+++ src/test/java/mapper/StudentMapperTest.java
@@ -36,13 +36,13 @@ public class StudentMapperTest {
      */
     @Test
     public void testLocalCache() throws Exception {
-        SqlSession sqlSession = factory.openSession(true); // 自动提交事务
+        SqlSession sqlSession = factory.openSession(true);// true为自动提交事务
         StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
-
+        //第一次经过数据库查询
+        System.out.println(studentMapper.getStudentById(1));
+        // 同一个sqlSession,走缓存
         System.out.println(studentMapper.getStudentById(1));
         System.out.println(studentMapper.getStudentById(1));
-        System.out.println(studentMapper.getStudentById(1));
-
         sqlSession.close();
     }
 
@@ -53,13 +53,13 @@ public class StudentMapperTest {
      */
     @Test
     public void testLocalCacheClear() throws Exception {
-        SqlSession sqlSession = factory.openSession(true); // 自动提交事务
+        SqlSession sqlSession = factory.openSession(true); // true为自动提交事务
         StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
-
         System.out.println(studentMapper.getStudentById(1));
+        // 自动提交事务，清空了缓存
         System.out.println("增加了" + studentMapper.addStudent(buildStudent()) + "个学生");
+        // 缓存为空，重新查询数据库
         System.out.println(studentMapper.getStudentById(1));
-
         sqlSession.close();
     }
 
@@ -70,18 +70,21 @@ public class StudentMapperTest {
      */
     @Test
     public void testLocalCacheScope() throws Exception {
-        SqlSession sqlSession1 = factory.openSession(true); // 自动提交事务
-        SqlSession sqlSession2 = factory.openSession(true); // 自动提交事务
-
+        SqlSession sqlSession1 = factory.openSession(true); // true为自动提交事务
+        SqlSession sqlSession2 = factory.openSession(true);
+        // 两个不同的sqlSession
        StudentMapper studentMapper = sqlSession1.getMapper(StudentMapper.class);
        StudentMapper studentMapper2 = sqlSession2.getMapper(StudentMapper.class);
-
+       // 第一次从数据库中读
         System.out.println("studentMapper读取数据: " + studentMapper.getStudentById(1));
+        // 一个session,从一级缓存中读
         System.out.println("studentMapper读取数据: " + studentMapper.getStudentById(1));
+        // 清空sqlSession2的一级缓存
         System.out.println("studentMapper2更新了" + studentMapper2.updateStudentName("小岑",1) + "个学生的数据");
+        // sqlSession1的一级缓存还在
         System.out.println("studentMapper读取数据: " + studentMapper.getStudentById(1));
+        // sqlSession2一级缓存没了，所以重新从数据库读
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentById(1));
-
     }
 
 
@@ -99,15 +102,16 @@ public class StudentMapperTest {
      */
     @Test
     public void testCacheWithoutCommitOrClose() throws Exception {
-        SqlSession sqlSession1 = factory.openSession(true); // 自动提交事务
-        SqlSession sqlSession2 = factory.openSession(true); // 自动提交事务
-
+        SqlSession sqlSession1 = factory.openSession(true); // true为自动提交事务
+        SqlSession sqlSession2 = factory.openSession(true);
+        // 两个不同的sqlSession
         StudentMapper studentMapper = sqlSession1.getMapper(StudentMapper.class);
         StudentMapper studentMapper2 = sqlSession2.getMapper(StudentMapper.class);
-
         System.out.println("studentMapper读取数据: " + studentMapper.getStudentById(1));
+        // Mapper的nameSpace相同，不管是不是同一个session，都可以用二级缓存
+        // 但是因为sqlSession1未提交，所以没有存入二级缓存。
+        // 只能从数据库查出
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentById(1));
-
     }
 
     /**
@@ -117,16 +121,15 @@ public class StudentMapperTest {
      */
     @Test
     public void testCacheWithCommitOrClose() throws Exception {
-        SqlSession sqlSession1 = factory.openSession(true); // 自动提交事务
-        SqlSession sqlSession2 = factory.openSession(true); // 自动提交事务
-
+        SqlSession sqlSession1 = factory.openSession(true); // true为自动提交事务
+        SqlSession sqlSession2 = factory.openSession(true);
         StudentMapper studentMapper = sqlSession1.getMapper(StudentMapper.class);
         StudentMapper studentMapper2 = sqlSession2.getMapper(StudentMapper.class);
-
         System.out.println("studentMapper读取数据: " + studentMapper.getStudentById(1));
+        // sqlSession1关闭或事务提交，则放入二级缓存
         sqlSession1.close();
+        // 从二级缓存中取出
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentById(1));
-
     }
 
     /**
@@ -136,22 +139,20 @@ public class StudentMapperTest {
      */
     @Test
     public void testCacheWithUpdate() throws Exception {
-        SqlSession sqlSession1 = factory.openSession(true); // 自动提交事务
-        SqlSession sqlSession2 = factory.openSession(true); // 自动提交事务
-        SqlSession sqlSession3 = factory.openSession(true); // 自动提交事务
-
-
+        SqlSession sqlSession1 = factory.openSession(true);
+        SqlSession sqlSession2 = factory.openSession(true);
+        SqlSession sqlSession3 = factory.openSession(true);
         StudentMapper studentMapper = sqlSession1.getMapper(StudentMapper.class);
         StudentMapper studentMapper2 = sqlSession2.getMapper(StudentMapper.class);
         StudentMapper studentMapper3 = sqlSession3.getMapper(StudentMapper.class);
-
-
         System.out.println("studentMapper读取数据: " + studentMapper.getStudentById(1));
         sqlSession1.close();
+        // sqlSession1关闭后，可从二级缓存中查出
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentById(1));
-
         studentMapper3.updateStudentName("方方",1);
+        // 提交会清空二级缓存
         sqlSession3.commit();
+        // 从数据库查
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentById(1));
     }
 
@@ -165,21 +166,17 @@ public class StudentMapperTest {
         SqlSession sqlSession1 = factory.openSession(true); // 自动提交事务
         SqlSession sqlSession2 = factory.openSession(true); // 自动提交事务
         SqlSession sqlSession3 = factory.openSession(true); // 自动提交事务
-
-
         StudentMapper studentMapper = sqlSession1.getMapper(StudentMapper.class);
         StudentMapper studentMapper2 = sqlSession2.getMapper(StudentMapper.class);
         ClassMapper classMapper = sqlSession3.getMapper(ClassMapper.class);
-
-
         System.out.println("studentMapper读取数据: " + studentMapper.getStudentByIdWithClassInfo(1));
         sqlSession1.close();
-
+        // 可从二级缓存中查出
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentByIdWithClassInfo(1));
-
+        // 提交只清空了classMapper的二级缓存，没有清空StudentMapper的二级缓存，所以读到了脏值
         classMapper.updateClassName("特色一班",1);
         sqlSession3.commit();
-
+        // 仍然从缓存中查出
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentByIdWithClassInfo(1));
     }
 
@@ -204,7 +201,7 @@ public class StudentMapperTest {
         sqlSession1.close();
 
         System.out.println("studentMapper2读取数据: " + studentMapper2.getStudentByIdWithClassInfo(1));
-
+        // classMapper的二级缓存的namespace跟studentMapper是一样的，所以把studentMapper的二级缓存也清空了
         classMapper.updateClassName("特色一班",1);
         sqlSession3.commit();
 
